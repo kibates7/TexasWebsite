ed65f8a5db444bdba3271ee237ecb038d0b335ce
Reginald Todd Randolph Jr <reginaldrandolph@my.unt.edu>
2018-07-23 13:39:13 -0500

    added overlay

---
+++ js/tx-overlay.js
@@ -0,0 +1,162 @@
+							
+			$("#topleft-grow").hover(function(){
+				//$("#texas-full").fadeIn(500); 
+				//$('#topleft-grow').animate({'opacity':'0'}, 100);
+				$('#top-grow').animate({'opacity':'0'}, 100);
+				$('#topright-grow').animate({'opacity':'0'}, 100);
+				$('#farleft-grow').animate({'opacity':'0'}, 100);
+				$('#left-grow').animate({'opacity':'0'}, 100);
+				$('#center-grow').animate({'opacity':'0'}, 100);
+				$('#right-grow').animate({'opacity':'0'}, 100);
+				$('#bottom-grow').animate({'opacity':'0'}, 100);
+			},function(){
+				//$('#texas-full').hide();
+				//$('#topleft-grow').animate({'opacity':'1'}, 100);
+				$('#top-grow').animate({'opacity':'1'}, 100);
+				$('#topright-grow').animate({'opacity':'1'}, 100);
+				$('#farleft-grow').animate({'opacity':'1'}, 100);
+				$('#left-grow').animate({'opacity':'1'}, 100);
+				$('#center-grow').animate({'opacity':'1'}, 100);
+				$('#right-grow').animate({'opacity':'1'}, 100);
+				$('#bottom-grow').animate({'opacity':'1'}, 100);
+			});
+
+			
+			$("#top-grow").hover(function(){
+				//$("#texas-full").fadeIn(500); 
+				$('#topleft-grow').animate({'opacity':'0'}, 100);
+				//$('#top-grow').animate({'opacity':'0'}, 100);
+				$('#topright-grow').animate({'opacity':'0'}, 100);
+				$('#farleft-grow').animate({'opacity':'0'}, 100);
+				$('#left-grow').animate({'opacity':'0'}, 100);
+				$('#center-grow').animate({'opacity':'0'}, 100);
+				$('#right-grow').animate({'opacity':'0'}, 100);
+				$('#bottom-grow').animate({'opacity':'0'}, 100);
+			},function(){
+				//$('#texas-full').hide();
+				$('#topleft-grow').animate({'opacity':'1'}, 100);
+				//$('#top-grow').animate({'opacity':'1'}, 100);
+				$('#topright-grow').animate({'opacity':'1'}, 100);
+				$('#farleft-grow').animate({'opacity':'1'}, 100);
+				$('#left-grow').animate({'opacity':'1'}, 100);
+				$('#center-grow').animate({'opacity':'1'}, 100);
+				$('#right-grow').animate({'opacity':'1'}, 100);
+				$('#bottom-grow').animate({'opacity':'1'}, 100);
+			});
+
+			
+			$("#topright-grow").hover(function(){
+				//$("#texas-full").fadeIn(500); 
+				$('#topleft-grow').animate({'opacity':'0'}, 100);
+				$('#top-grow').animate({'opacity':'0'}, 100);
+				//$('#topright-grow').animate({'opacity':'0'}, 100);
+				$('#farleft-grow').animate({'opacity':'0'}, 100);
+				$('#left-grow').animate({'opacity':'0'}, 100);
+				$('#center-grow').animate({'opacity':'0'}, 100);
+				$('#right-grow').animate({'opacity':'0'}, 100);
+				$('#bottom-grow').animate({'opacity':'0'}, 100);
+			},function(){
+				//$('#texas-full').hide();
+				$('#topleft-grow').animate({'opacity':'1'}, 100);
+				$('#top-grow').animate({'opacity':'1'}, 100);
+				//$('#topright-grow').animate({'opacity':'1'}, 100);
+				$('#farleft-grow').animate({'opacity':'1'}, 100);
+				$('#left-grow').animate({'opacity':'1'}, 100);
+				$('#center-grow').animate({'opacity':'1'}, 100);
+				$('#right-grow').animate({'opacity':'1'}, 100);
+				$('#bottom-grow').animate({'opacity':'1'}, 100);
+			});
+
+			
+			$("#farleft-grow").hover(function(){
+				//$("#texas-full").fadeIn(500); 
+				$('#topleft-grow').animate({'opacity':'0'}, 100);
+				$('#top-grow').animate({'opacity':'0'}, 100);
+				$('#topright-grow').animate({'opacity':'0'}, 100);
+				//$('#farleft-grow').animate({'opacity':'0'}, 100);
+				$('#left-grow').animate({'opacity':'0'}, 100);
+				$('#center-grow').animate({'opacity':'0'}, 100);
+				$('#right-grow').animate({'opacity':'0'}, 100);
+				$('#bottom-grow').animate({'opacity':'0'}, 100);
+			},function(){
+				//$('#texas-full').hide();
+				$('#topleft-grow').animate({'opacity':'1'}, 100);
+				$('#top-grow').animate({'opacity':'1'}, 100);
+				$('#topright-grow').animate({'opacity':'1'}, 100);
+				//$('#farleft-grow').animate({'opacity':'1'}, 100);
+				$('#left-grow').animate({'opacity':'1'}, 100);
+				$('#center-grow').animate({'opacity':'1'}, 100);
+				$('#right-grow').animate({'opacity':'1'}, 100);
+				$('#bottom-grow').animate({'opacity':'1'}, 100);
+			});
+
+			
+			$("#left-grow").hover(function(){
+				//$("#texas-full").fadeIn(500); 
+				$('#topleft-grow').animate({'opacity':'0'}, 100);
+				$('#top-grow').animate({'opacity':'0'}, 100);
+				$('#topright-grow').animate({'opacity':'0'}, 100);
+				$('#farleft-grow').animate({'opacity':'0'}, 100);
+				//$('#left-grow').animate({'opacity':'0'}, 100);
+				$('#center-grow').animate({'opacity':'0'}, 100);
+				$('#right-grow').animate({'opacity':'0'}, 100);
+				$('#bottom-grow').animate({'opacity':'0'}, 100);
+			},function(){
+				//$('#texas-full').hide();
+				$('#topleft-grow').animate({'opacity':'1'}, 100);
+				$('#top-grow').animate({'opacity':'1'}, 100);
+				$('#topright-grow').animate({'opacity':'1'}, 100);
+				$('#farleft-grow').animate({'opacity':'1'}, 100);
+				//$('#left-grow').animate({'opacity':'1'}, 100);
+				$('#center-grow').animate({'opacity':'1'}, 100);
+				$('#right-grow').animate({'opacity':'1'}, 100);
+				$('#bottom-grow').animate({'opacity':'1'}, 100);
+			});
+			
+			
+			$("#right-grow").hover(function(){
+				//$("#texas-full").fadeIn(500); 
+				$('#topleft-grow').animate({'opacity':'0'}, 100);
+				$('#top-grow').animate({'opacity':'0'}, 100);
+				$('#topright-grow').animate({'opacity':'0'}, 100);
+				$('#farleft-grow').animate({'opacity':'0'}, 100);
+				$('#left-grow').animate({'opacity':'0'}, 100);
+				$('#center-grow').animate({'opacity':'0'}, 100);
+				//$('#right-grow').animate({'opacity':'0'}, 100);
+				$('#bottom-grow').animate({'opacity':'0'}, 100);
+			},function(){
+				//$('#texas-full').hide();
+				$('#topleft-grow').animate({'opacity':'1'}, 100);
+				$('#top-grow').animate({'opacity':'1'}, 100);
+				$('#topright-grow').animate({'opacity':'1'}, 100);
+				$('#farleft-grow').animate({'opacity':'1'}, 100);
+				$('#left-grow').animate({'opacity':'1'}, 100);
+				$('#center-grow').animate({'opacity':'1'}, 100);
+				//$('#right-grow').animate({'opacity':'1'}, 100);
+				$('#bottom-grow').animate({'opacity':'1'}, 100);
+			});
+			
+			
+			 $("#bottom-grow").hover(function(){
+				//$("#texas-full").fadeIn(500); 
+				$('#topleft-grow').animate({'opacity':'0'}, 100);
+				$('#top-grow').animate({'opacity':'0'}, 100);
+				$('#topright-grow').animate({'opacity':'0'}, 100);
+				$('#farleft-grow').animate({'opacity':'0'}, 100);
+				$('#left-grow').animate({'opacity':'0'}, 100);
+				$('#center-grow').animate({'opacity':'0'}, 100);
+				$('#right-grow').animate({'opacity':'0'}, 100);
+				//$('#bottom-grow').animate({'opacity':'0'}, 100);
+			},function(){
+				//$('#texas-full').hide();
+				$('#topleft-grow').animate({'opacity':'1'}, 100);
+				$('#top-grow').animate({'opacity':'1'}, 100);
+				$('#topright-grow').animate({'opacity':'1'}, 100);
+				$('#farleft-grow').animate({'opacity':'1'}, 100);
+				$('#left-grow').animate({'opacity':'1'}, 100);
+				$('#center-grow').animate({'opacity':'1'}, 100);
+				$('#right-grow').animate({'opacity':'1'}, 100);
+				//$('#bottom-grow').animate({'opacity':'1'}, 100);
+			});
+
+		
